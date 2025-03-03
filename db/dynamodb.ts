a190f45aaf2924d765c63b04c86bfc527bb848a7
adyrmishi <angeladyrmishi@outlook.com>
2025-03-03 15:45:11 +0000

    compile again

--- db/dynamodb.ts
+++ db/dynamodb.ts
@@ -2,16 +2,10 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import dotenv from "dotenv";
 
 dotenv.config();
-if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
-  throw new Error("AWS credentials must be set.");
-}
+
 const dynamoDB = new DynamoDBClient({
   region: process.env.AWS_DEFAULT_REGION,
   endpoint: process.env.DYNAMODB_ENDPOINT || "http://localhost:8000",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
 });
 
 export default dynamoDB;
--- dist/db/dynamodb.js
+++ dist/db/dynamodb.js
@@ -6,15 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const client_dynamodb_1 = require("@aws-sdk/client-dynamodb");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
-    throw new Error("AWS credentials must be set.");
-}
 const dynamoDB = new client_dynamodb_1.DynamoDBClient({
     region: process.env.AWS_DEFAULT_REGION,
     endpoint: process.env.DYNAMODB_ENDPOINT || "http://localhost:8000",
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    },
 });
 exports.default = dynamoDB;
