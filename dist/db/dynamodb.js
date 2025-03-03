544776bed765fd94381e4f27887dfbe934a3b5f0
adyrmishi <angeladyrmishi@outlook.com>
2025-03-03 17:19:50 +0000

    precompile

--- dist/db/dynamodb.js
+++ dist/db/dynamodb.js
@@ -8,6 +8,8 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const dynamoDB = new client_dynamodb_1.DynamoDBClient({
     region: process.env.AWS_DEFAULT_REGION || "eu-west-2",
-    endpoint: process.env.DYNAMODB_ENDPOINT || "http://localhost:8000",
+    endpoint: process.env.NODE_ENV === "production"
+        ? undefined
+        : process.env.DYNAMODB_ENDPOINT || "http://localhost:8000",
 });
 exports.default = dynamoDB;
